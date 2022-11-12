9dc7387c672663910675c3f0c346676c4a0095e5
ftc <17615007230@163.com>
2022-11-12 10:53:09 +0800

    chore(启动类): 修改代码注释
    1.修改代码注释

--- src/main/java/com/ftc/threadtest/ThreadTestApplication.java
+++ src/main/java/com/ftc/threadtest/ThreadTestApplication.java
@@ -3,11 +3,14 @@ package com.ftc.threadtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: 冯铁城 [17615007230@163.com]
+ * @date: 2022-11-08 18:58:51
+ * @describe: 项目启动类
+ */
 @SpringBootApplication
 public class ThreadTestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ThreadTestApplication.class, args);
     }
-
 }
