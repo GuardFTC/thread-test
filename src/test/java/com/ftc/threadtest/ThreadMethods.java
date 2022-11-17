7e29aa17cc3200398dcd6518c2170b596e818866
ftc <17615007230@163.com>
2022-11-17 10:38:27 +0800

    test(单元测试): 取消部分代码屏蔽
    1.取消部分代码屏蔽

--- src/test/java/com/ftc/threadtest/ThreadMethods.java
+++ src/test/java/com/ftc/threadtest/ThreadMethods.java
@@ -1,5 +1,8 @@
 package com.ftc.threadtest;
 
+import cn.hutool.core.lang.Assert;
+import com.ftc.threadtest.deal.People;
+import com.ftc.threadtest.deal.Receiver;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
@@ -11,73 +14,73 @@ import java.util.concurrent.TimeUnit;
  * @describe: Wait和Sleep方法
  */
 public class ThreadMethods {
-//
-//    @Test
-//    @SneakyThrows
-//    void testWaitAndSleep() {
-//
-//        //1.创建人类对象
-//        People people = new People();
-//
-//        //2.创建3个线程
-//        Thread thread1 = new Thread(new Receiver(people, "玩手机"), Receiver.PLAY_PHONE);
-//        Thread thread2 = new Thread(new Receiver(people, "刷牙"), Receiver.BRUSH_TEETH);
-//        Thread thread3 = new Thread(new Receiver(people, "睡觉"), Receiver.SLEEP);
-//
-//        //3.开启线程
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
-//
-//        //4.睡1s，确保其他线程能够运行完毕
-//        TimeUnit.SECONDS.sleep(3);
-//    }
-//
-//    @Test
-//    void testInterrupted() {
-//
-//        //1.创建自定义线程
-//        Thread customThread = new Thread(() -> {
-//            System.out.println(Thread.currentThread().getName() + " is running");
-//        });
-//
-//        //2.运行自定义线程
-//        customThread.start();
-//
-//        //3.设置自定义线程中断标识为true
-//        customThread.interrupt();
-//
-//        //4.校验自定义线程中断标识是否为true
-//        Assert.isTrue(customThread.isInterrupted());
-//
-//        //5.判定当前线程中断标识是否为true
-//        Thread.currentThread().interrupt();
-//        Assert.isTrue(Thread.interrupted());
-//        Assert.isFalse(Thread.currentThread().isInterrupted());
-//    }
-//
-//    @Test
-//    void testYield() {
-//
-//        //1.创建第一个线程
-//        new Thread(() -> {
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(Thread.currentThread().getName() + "->" + i);
-//
-//                //2.i==2时，让出CPU，重新进入Ready状态
-//                if (i == 2) {
-//                    Thread.yield();
-//                }
-//            }
-//        }).start();
-//
-//        //2.创建第二个线程
-//        new Thread(() -> {
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(Thread.currentThread().getName() + "->" + i);
-//            }
-//        }).start();
-//    }
+
+    @Test
+    @SneakyThrows
+    void testWaitAndSleep() {
+
+        //1.创建人类对象
+        People people = new People();
+
+        //2.创建3个线程
+        Thread thread1 = new Thread(new Receiver(people, "玩手机"), Receiver.PLAY_PHONE);
+        Thread thread2 = new Thread(new Receiver(people, "刷牙"), Receiver.BRUSH_TEETH);
+        Thread thread3 = new Thread(new Receiver(people, "睡觉"), Receiver.SLEEP);
+
+        //3.开启线程
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        //4.睡1s，确保其他线程能够运行完毕
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    @Test
+    void testInterrupted() {
+
+        //1.创建自定义线程
+        Thread customThread = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " is running");
+        });
+
+        //2.运行自定义线程
+        customThread.start();
+
+        //3.设置自定义线程中断标识为true
+        customThread.interrupt();
+
+        //4.校验自定义线程中断标识是否为true
+        Assert.isTrue(customThread.isInterrupted());
+
+        //5.判定当前线程中断标识是否为true
+        Thread.currentThread().interrupt();
+        Assert.isTrue(Thread.interrupted());
+        Assert.isFalse(Thread.currentThread().isInterrupted());
+    }
+
+    @Test
+    void testYield() {
+
+        //1.创建第一个线程
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "->" + i);
+
+                //2.i==2时，让出CPU，重新进入Ready状态
+                if (i == 2) {
+                    Thread.yield();
+                }
+            }
+        }).start();
+
+        //2.创建第二个线程
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "->" + i);
+            }
+        }).start();
+    }
 
     @Test
     @SneakyThrows(InterruptedException.class)
