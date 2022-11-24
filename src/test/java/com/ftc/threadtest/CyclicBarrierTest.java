1d1cf0b7598ca6c22fff497fa145376f46f07d3f
ftc <17615007230@163.com>
2022-11-24 15:20:52 +0800

    chore(单元测试): CyclicBarrier单元测试优化
    1.CyclicBarrier单元测试优化

--- src/test/java/com/ftc/threadtest/CyclicBarrierTest.java
+++ src/test/java/com/ftc/threadtest/CyclicBarrierTest.java
@@ -1,10 +1,9 @@
 package com.ftc.threadtest;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.*;
 
 /**
  * @author: 冯铁城 [17615007230@163.com]
@@ -20,12 +19,12 @@ public class CyclicBarrierTest {
         CountDownLatch countDownLatch = new CountDownLatch(3);
 
         //2.创建循环栅栏，阻塞等待
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(3,()->{
-            System.out.println("所有玩家已经进入游戏！");
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, () -> {
+            System.out.println(Thread.currentThread().getName() + ":所有玩家已经进入游戏！");
         });
 
         //3.开始考试
-        System.out.println("等待玩家进入");
+        System.out.println(Thread.currentThread().getName() + ":等待玩家进入");
 
         //4.线程1进入游戏
         new Thread(() -> {
@@ -71,6 +70,94 @@ public class CyclicBarrierTest {
         }
 
         //8.输出
-        System.out.println("全部玩家已进入，开始游戏！！");
+        System.out.println(Thread.currentThread().getName() + ":全部玩家已进入，开始游戏！！");
+    }
+
+    @Test
+    @SneakyThrows(InterruptedException.class)
+    void testReset() {
+
+        //1.创建循环栅栏，阻塞等待
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, () -> {
+            System.out.println("开始点菜！");
+        });
+
+        //2.创建线程1开始等待
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 已到达餐厅");
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                System.out.println(Thread.currentThread().getName() + " 饭局散了");
+            }
+        }).start();
+
+        //3.创建线程2开始等待
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 已到达餐厅");
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                System.out.println(Thread.currentThread().getName() + " 饭局散了");
+            }
+        }).start();
+
+        //4.创建线程3进行reset操作
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 抱歉了我去不了了");
+            cyclicBarrier.reset();
+        }).start();
+
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    @Test
+    @SneakyThrows(InterruptedException.class)
+    void testAwaitTimeOut() {
+
+        //1.创建循环栅栏，阻塞等待
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, () -> {
+            System.out.println("开始点菜！");
+        });
+
+        //2.创建线程1开始等待
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 已到达餐厅");
+            try {
+                cyclicBarrier.await(100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
+            }
+            System.out.println(Thread.currentThread().getName() + " 不等了！开吃开吃！");
+        }).start();
+
+        //3.创建线程2开始等待
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 已到达餐厅");
+            try {
+                cyclicBarrier.await(100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
+            }
+            System.out.println(Thread.currentThread().getName() + " 不等了！开吃开吃！");
+        }).start();
+
+        //4.创建线程3延迟进行await操作
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " 大家等我一会，我比较慢");
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                System.out.println(Thread.currentThread().getName() + " 你们竟然不等我，伤心了");
+            }
+        }).start();
+
+        //5.睡一会，确保子线程执行完成
+        TimeUnit.SECONDS.sleep(2);
     }
 }
